27e8c561f44f290dd46a4bc44188ae006a1ae192
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-09-16 21:33:09 +0200

    fix registry name

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ REGISTRY=docker.pkg.github.com
 IMAGE_TAG=$REGISTRY/$GITHUB_REPOSITORY_OWNER/$GIHUB_REPOSITORY_NAME/$GIHUB_REPOSITORY_NAME:$INPUT_TAG
 
 echo "Authenticating into $INPUT_REGISTRY as $GITHUB_REPOSITORY_OWNER..."
-echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $GITHUB_REPOSITORY_OWNER -password-stdin
+echo $INPUT_PASSWORD | docker login $REGISTRY -u $GITHUB_REPOSITORY_OWNER -password-stdin
 echo "Building $IMAGE_TAG..."
 docker build -t $IMAGE_TAG .
 echo "Pushing $IMAGE_TAG..."
