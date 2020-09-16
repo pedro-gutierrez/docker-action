b49d5d3499210a0dd4531bdd5178ed7d3c79c376
Pedro Gutiérrez <pedro.gutierrez@form3.tech>
2020-09-16 20:30:03 +0200

    fix image tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 echo "Authenticating into $INPUT_REGISTRY as $INPUT_USERNAME..."
 echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
-echo "Building $INPUT_IMAGE..."
-docker build -t $INPUT_IMAGE .
-echo "Pushing $INPUT_IMAGE to $INPUT_REGISTRY..."
-docker push $INPUT_REGISTRY/$INPUT_USERNAME/$INPUT_IMAGE:$INPUT_TAG
+FULL_IMAGE_TAG=$INPUT_REGISTRY/$INPUT_USERNAME/$INPUT_IMAGE:$INPUT_TAG
+echo "Building $FULL_IMAGE_TAG..."
+docker build -t $FULL_IMAGE_TAG .
+echo "Pushing $FULL_IMAGE_TAG..."
+docker push $FULL_IMAGE_TAG
